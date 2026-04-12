786ab1165eada762945e9e36170a2108d6ffce9c
Hassan <hassanjrao@gmail.com>
2026-04-12 23:58:21 +0500

    return in process

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -349,7 +349,8 @@ class FrontendController extends Controller
         // Send email notification to the property owner
         // try {
         // AlexLluch3@gmail.com
-        $email = 'AlexLluch3@gmail.com';
+        // $email = 'AlexLluch3@gmail.com';
+        $email = config('mail.from.address');
             Mail::to($email)->send(new ContactFormMail($validated));
         // } catch (\Exception $e) {
         //     Log::error('Error sending contact email: ', [
